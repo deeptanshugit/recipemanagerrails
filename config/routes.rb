b9f0f7534f2e201edfbace6ccbc52c4e63908886
deeptanshugit <deeptanshu@acornglobus.com>
2023-09-19 17:53:58 +0530

    Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
-  # Defines the root path route ("/")
-  root "static_pages#home"
 end
